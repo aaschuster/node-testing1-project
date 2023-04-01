3078f3b9b35b2c49b19b5fafe72f40cb707e72ca
Aaron Schuster <aaronjschuster@gmail.com>
2023-04-01 15:27:03 -0400

    write test 18

--- index.test.js
+++ index.test.js
@@ -114,7 +114,10 @@ describe('[Exercise 6] Car', () => {
     focus.drive(100);
     expect(focus.odometer).toBe(20*30 + 100);
   })
-  test('[18] adding fuel to a full tank has no effect', () => {})
+  test('[18] adding fuel to a full tank has no effect', () => {
+    focus.refuel(20);
+    expect(focus.tank).toBe(20);
+  })
 })
 
 // describe('[Exercise 7] isEvenNumberAsync', () => {
